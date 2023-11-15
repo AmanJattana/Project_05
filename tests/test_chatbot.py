a6d195a368b2817987fd22c764e94018c0d67310
AmanJattana <amanjattana1997@gmail.com>
2023-11-15 12:07:06 -0800

    Initial commit: Set up project directory and file structure

---
+++ tests/test_chatbot.py
@@ -0,0 +1,11 @@
+"""
+Description: creating a chatbot for the pixell river transaction  
+Author:Aman
+Date:15-11-2023
+Usage: prompts the user for various inputs and performs functions. 
+"""
+import unittest
+from unittest.mock import patch
+
+
+
